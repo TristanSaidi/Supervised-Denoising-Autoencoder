db9a010675c7ebb684224190d7b0de173dc2ada8
TristanSaidi <tls2160@columbia.edu>
2022-11-10 20:17:42 -0500

    Initial files

---
+++ src/siamese_network.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+import torch as th
+import torch.nn as nn
+
+
+def contrastive_loss(y1, y2, flag):
+    '''
+    flag indicates whether y1,y2 belong to the same class
+    '''
+
+class SiameseNetwork(nn.Module):
+    '''
+    Siamese network to generate embeddings for our labels
+    '''
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+
