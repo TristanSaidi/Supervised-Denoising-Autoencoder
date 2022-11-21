a345302f53e097ffce3a8339b25fb9b9801dbe07
TristanSaidi <tls2160@columbia.edu>
2022-11-21 09:42:39 -0500

    modified data_loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,103 +1,25 @@
-import os
-import pickle
-import sys
-import tarfile
-
 import numpy as np
-from PIL import Image
-from six.moves.urllib.request import urlretrieve
 
+import torch as th
+import torchvision
+from torchvision.datasets import CIFAR100
+import torchvision.transforms as tt
 
-## Largely pulled from starter code for HW 2 of Richard Zemel's Neural Networks and Deep Learning class at Columbia
+stats = ((0.5074,0.4867,0.4411),(0.2011,0.1987,0.2025))
 
-def get_file(fname, origin, untar=False, extract=False, archive_format="auto", cache_dir="data"):
-    datadir = os.path.join(cache_dir)
-    if not os.path.exists(datadir):
-        os.makedirs(datadir)
+train_transform = tt.Compose([
+    tt.RandomHorizontalFlip(),
+    tt.RandomCrop(32,padding=4,padding_mode="reflect"),
+    tt.ToTensor(),
+    tt.Normalize(*stats)
+])
 
-    if untar:
-        untar_fpath = os.path.join(datadir, fname)
-        fpath = untar_fpath + ".tar.gz"
-    else:
-        fpath = os.path.join(datadir, fname)
+test_transform = tt.Compose([
+    tt.ToTensor(),
+    tt.Normalize(*stats)
+])
 
-    print("File path: %s" % fpath)
-    if not os.path.exists(fpath):
-        print("Downloading data from", origin)
-
-        error_msg = "URL fetch failure on {}: {} -- {}"
-        try:
-            try:
-                urlretrieve(origin, fpath)
-            except URLError as e:
-                raise Exception(error_msg.format(origin, e.errno, e.reason))
-            except HTTPError as e:
-                raise Exception(error_msg.format(origin, e.code, e.msg))
-        except (Exception, KeyboardInterrupt) as e:
-            if os.path.exists(fpath):
-                os.remove(fpath)
-            raise
-
-    if untar:
-        if not os.path.exists(untar_fpath):
-            print("Extracting file.")
-            with tarfile.open(fpath) as archive:
-                archive.extractall(datadir)
-        return untar_fpath
-
-    if extract:
-        _extract_archive(fpath, datadir, archive_format)
-
-    return fpath
-
-
-
-def load_batch(fpath, label_key="fine_labels"):
-    """Internal utility for parsing CIFAR data.
-    # Arguments
-        fpath: path the file to parse.
-        label_key: key for label data in the retrieve
-            dictionary.
-    # Returns
-        A tuple `(data, labels)`.
-    """
-    f = open(fpath, "rb")
-    if sys.version_info < (3,):
-        d = pickle.load(f)
-    else:
-        d = pickle.load(f, encoding="bytes")
-        # decode utf8
-        d_decoded = {}
-        for k, v in d.items():
-            d_decoded[k.decode("utf8")] = v
-        d = d_decoded
-    f.close()
-    data = d["data"]
-    labels = d[label_key]
-
-    data = data.reshape(data.shape[0], 3, 32, 32)
-    return data, labels
-
-
-def load_cifar100(transpose=False):
-    """Loads CIFAR10 dataset.
-    # Returns
-        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
-    """
-    dirname = "cifar-100-python"
-    origin = "http://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz"
-    path = get_file(dirname, origin=origin, untar=True)
-
-    fpath = os.path.join(path, "train")
-    x_train, y_train = load_batch(fpath)
-
-    fpath = os.path.join(path, "test")
-    x_test, y_test = load_batch(fpath)
-
-    y_train = np.reshape(y_train, (len(y_train), 1))
-    y_test = np.reshape(y_test, (len(y_test), 1))
-
-    if transpose:
-        x_train = x_train.transpose(0, 2, 3, 1)
-        x_test = x_test.transpose(0, 2, 3, 1)
-    return (x_train, y_train), (x_test, y_test)
+if __name__ == "__main__":
+    train_data = CIFAR100(download=True,root="./data",transform=train_transform)
+    print(train_data[0])
+    test_data = CIFAR100(root="./data",train=False,transform=test_transform)
